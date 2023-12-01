847133e077b059f2b99847232dc1ed243d94422a
Tony Eder <tonyeder11@gmail.com>
2023-12-01 15:10:24 -0800

    more practice here to dial in the syntax

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
 // Code Keypad Component Here
 
-function Keypad (){
+function Keypad() {
+    function handleKeypad(event) {
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input
+                type="password"
+                id="PASSWORD"
+                placeholder="Enter Password..."
+                onChange={handleKeypad}
+            />
+        </div>
     )
 }
 
